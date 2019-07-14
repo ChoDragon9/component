5be86a23d37dda2ae2f0a49c1027050672a7a46b
peter.cho <peter.cho@kakaocorp.com>
2019-07-14 19:23:34 +0900

    수정

--- src/sketchbook/store.js
+++ src/sketchbook/store.js
@@ -2,35 +2,36 @@ import {createStore} from '../core/store'
 import {load} from "./helper";
 
 let state = {
-  coordinates: [
-    'defaultRect',
-    'defaultTriangle'
-  ],
-  defaultRect: [
-	  [100, 100],
-	  [100, 200],
-	  [200, 200],
-	  [200, 100]
-  ],
-  defaultTriangle: [
-	  [350, 100],
-	  [300, 200],
-	  [400, 200]
-  ],
-  selectedPoint: {
-    index: null,
-    key: null
-  },
-  selectedPolygon: {
-    key: null
-  },
-  prevCoordinate: null
+	coordinates: [
+		'defaultRect',
+		'defaultTriangle',
+		'defaultStar'
+	],
+	defaultRect: [
+		[100, 100],
+		[100, 200],
+		[200, 200],
+		[200, 100]
+	],
+	defaultTriangle: [
+		[350, 100],
+		[300, 200],
+		[400, 200]
+	],
+	defaultStar: [[555, 89], [538, 124], [499, 128], [521, 162], [505, 189], [538, 188], [554, 220], [576, 186], [611, 185], [587, 152], [612, 123], [572, 125]],
+	selectedPoint: {
+		index: null,
+		key: null
+	},
+	selectedPolygon: {
+		key: null
+	},
+	prevCoordinate: null
 }
 
 const storageState = load()
 if (storageState) {
 	state = {...state, ...storageState}
-	console.log(state)
 }
 
 export const store = createStore(state)
