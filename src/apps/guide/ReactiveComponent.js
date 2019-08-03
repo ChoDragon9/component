a17d58493d57885068020a4e02d3e8d81553e5e5
peter.cho <peter.cho@kakaocorp.com>
2019-08-03 13:43:07 +0900

    반응형 예제 추가

--- src/apps/guide/ReactiveComponent.js
+++ src/apps/guide/ReactiveComponent.js
@@ -4,7 +4,7 @@ export const ReactiveComponent = component({
   data () {
     return {
       message: 'message',
-      arr: ['1', '2'],
+      arr: [1, 2],
       obj: {
         key: 'value'
       },
@@ -22,8 +22,14 @@ export const ReactiveComponent = component({
     } = data
     return `<div>
 			<h1>Reactive Component</h1>
+			<button class="message-btn">Change Message</button>
+			<button class="arr-btn">Add Arr</button>
+			<button class="obj-btn">Change Obj</button>
+			<button class="bool-btn">Toggle Bool</button>
+			<button class="number-btn">Up Count</button>
+			<hr>
 			<ul>
-			  <li>Message: ${message} <button class="now">Now!</button></li>
+			  <li>Message: ${message}</li>
 			  <li>Arr: ${arr}</li>
 			  <li>Obj: ${JSON.stringify(obj)}</li>
 			  <li>Boolean: ${bool ? 'True' : 'False'}</li>
@@ -33,14 +39,33 @@ export const ReactiveComponent = component({
   },
   events () {
     return [
-      ['button.now', 'onclick', 'changeMessage']
+      ['button.message-btn', 'onclick', 'changeMessage'],
+	    ['button.arr-btn', 'onclick', 'changeArr'],
+	    ['button.obj-btn', 'onclick', 'changeObj'],
+	    ['button.bool-btn', 'onclick', 'toggle'],
+	    ['button.number-btn', 'onclick', 'upCount'],
     ]
   },
   methods ({store}) {
     return {
       changeMessage () {
-        console.log(Date.now())
         store.set('message', 'Now ' + Date.now())
+      },
+	    changeArr () {
+		    const arr = store.get('arr')
+		    arr.push(arr[arr.length - 1] + 1)
+		    store.set('arr', arr)
+	    },
+	    changeObj () {
+		    const obj = store.get('obj')
+        Object.assign(obj, {key: Date.now()})
+		    store.set('obj', obj)
+	    },
+      toggle () {
+	      store.set('bool', !store.get('bool'))
+      },
+	    upCount () {
+		    store.set('number', store.get('number') + 1)
       }
     }
   }
