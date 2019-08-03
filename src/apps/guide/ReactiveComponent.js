7329b839928cb737fc5f47fb3b773ae560651e9e
peter.cho <peter.cho@kakaocorp.com>
2019-08-03 14:14:40 +0900

    자식 컴포넌트 반응형 확인

--- src/apps/guide/ReactiveComponent.js
+++ src/apps/guide/ReactiveComponent.js
@@ -1,72 +1,33 @@
 import {component} from '../../core/component'
+import {NowComponent} from "./NowComponent";
 
 export const ReactiveComponent = component({
   data () {
     return {
-      message: 'message',
-      arr: [1, 2],
-      obj: {
-        key: 'value'
-      },
-      bool: false,
-      number: 0
+      now: Date.now()
     }
   },
   template ({data}) {
-    const {
-      message,
-      arr,
-      obj,
-      bool,
-      number
-    } = data
     return `<div>
-			<h1>Reactive Component</h1>
-			<button class="message-btn">Change Message</button>
-			<button class="arr-btn">Add Arr</button>
-			<button class="obj-btn">Change Obj</button>
-			<button class="bool-btn">Toggle Bool</button>
-			<button class="number-btn">Up Count</button>
-			<hr>
-			<ul>
-			  <li>Message: ${message}</li>
-			  <li>Arr: ${arr}</li>
-			  <li>Obj: ${JSON.stringify(obj)}</li>
-			  <li>Boolean: ${bool ? 'True' : 'False'}</li>
-			  <li>Number: ${number}</li>
-      </ul>
+			<now props="now"></now>
+			<button class="now-btn">Change Now</button>
 		</div>`
   },
+	components () {
+  	return [
+  	  ['now', NowComponent]
+	  ]
+	},
   events () {
     return [
-      ['button.message-btn', 'onclick', 'changeMessage'],
-	    ['button.arr-btn', 'onclick', 'changeArr'],
-	    ['button.obj-btn', 'onclick', 'changeObj'],
-	    ['button.bool-btn', 'onclick', 'toggle'],
-	    ['button.number-btn', 'onclick', 'upCount'],
+      ['button.now-btn', 'onclick', 'changeNow'],
     ]
   },
   methods ({store}) {
     return {
-      changeMessage () {
-        store.set('message', 'Now ' + Date.now())
+      changeNow () {
+        store.set('now', Date.now())
       },
-	    changeArr () {
-		    const arr = store.get('arr')
-		    arr.push(arr[arr.length - 1] + 1)
-		    store.set('arr', arr)
-	    },
-	    changeObj () {
-		    const obj = store.get('obj')
-        Object.assign(obj, {key: Date.now()})
-		    store.set('obj', obj)
-	    },
-      toggle () {
-	      store.set('bool', !store.get('bool'))
-      },
-	    upCount () {
-		    store.set('number', store.get('number') + 1)
-      }
     }
   }
 })
